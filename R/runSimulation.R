d70ec361d7518c338f2dd4873881445462d1362d
“Matthew <mherndon@ou.edu>
2020-03-04 14:36:03 -0600

    Still working to export app

---
+++ R/runSimulation.R
@@ -0,0 +1,8 @@
+#' @export
+runSimulation <- function() {
+  appDir <- system.file("shiny-examples", "distsim", package = "distsim")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)
+  }
+  shiny::runApp(appDir, display.mode = "normal")
+}
