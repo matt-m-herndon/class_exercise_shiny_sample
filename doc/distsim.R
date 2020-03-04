6470c967a7a7209683249444d46444ed16700c9c
“Matthew <mherndon@ou.edu>
2020-03-04 15:53:28 -0600

    added doc folder

---
+++ doc/distsim.R
@@ -0,0 +1,18 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  devtools::install_github('matt-m-herndon/class_exercise_shiny_sample')
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  distsim::runSimulation()
+
+## ----eval=FALSE---------------------------------------------------------------
+#  gd = g + stat_density2d(aes(colour=..density..), geom='point', contour=F) + scale_color_viridis()
+
+## ----eval=FALSE---------------------------------------------------------------
+#  ad = a + geom_density(fill="red")
+
